f83c9ba436beafa8b3bf90dca12d584213bc034c
AI-wgy <877311309@qq.com>
2020-08-19 20:36:10 +0800

    login-basic

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Main from '../views/Main.vue'
 
 import CategoryEdit from '../views/CategoryEdit.vue'
@@ -24,6 +24,10 @@ import AdminUserList from '../views/AdminUserList.vue'
 Vue.use(VueRouter)
 
   const routes = [
+  {
+    path: '/login', name: 'login', component: Login
+  },
+
   {
     path: '/',
     name: 'main',
