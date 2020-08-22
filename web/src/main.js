552f7b20c2ec5023ff5ede80dc4d4e622befcb8d
AI-wgy <877311309@qq.com>
2020-08-22 21:43:08 +0800

    web

--- web/src/main.js
+++ web/src/main.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import './style.scss'
+import router from './router'
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/swiper.scss'
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <img class="w-100" src="../assets/90bc94bd22057b3c7b9aa49bbd7d6e69.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/8aaa2cffbcaf1793b94844a64886a925.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/0aa5b55b6328398bb4e381f61621f463.jpeg" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.pagination-home'
+        }
+      }
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+  .pagination-home {
+    .swiper-pagination-bullet {
+      background: #fff;
+    }
+  }
+</style>
