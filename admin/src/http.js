730819c0166a8246f554f63f6e10a85568529387
AI-wgy <877311309@qq.com>
2020-08-27 16:18:38 +0800

    final

--- admin/src/http.js
+++ admin/src/http.js
@@ -3,7 +3,13 @@ import Vue from 'vue'
 import router from './router'
 
 const http = axios.create({
-    baseURL:'http://localhost:3000/admin/api'
+    // 接口需要根据开发和编译进行替换，VUE-APP有一种方法就是在编译的时候可以添加一些变量/环境
+    // 必须以VUE_APP开头API_URL自定义名字
+    //生产环境第一步
+    baseURL: process.env.VUE_APP_API_URL || '/admin/api'
+    // baseURL:'http://localhost:3000/admin/api'
+
+    // 第二步添加一个环境变量文件
 })
 
 //查授权信息
---
+++ web/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //输出的文件夹
+    outputDir: __dirname + '/../server/web',
+    //生成的静态文件路径
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/web/'
+    //   : '/'
+}
